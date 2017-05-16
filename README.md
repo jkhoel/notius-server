38b674180a5accec4f626a83b0ae117c7fe1a840
Jens Kristian Hoel <github@hoelweb.com>
2017-05-16 19:41:23 +0200

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # notius-server
-The server app used for the notius-ui project
+The server app used for the notius-ui project.
+
+It is a node.js server that connects to DCS:World via mint.lua and broadcasts a JSON object
---
+++ server/dcsdataretriever.js
@@ -0,0 +1,42 @@
+module.exports = function DCSDataRetriever(dataCallback) {
+
+    const PORT = 3001;
+    const ADDRESS = "127.0.0.1";
+    //const ADDRESS = "89.11.174.88";
+    //const ADDRESS = "51.175.54.160";
+    //const PORT = 10308;
+    //const PORT = 135;
+
+    const net = require('net');
+    let buffer;
+
+    function connect() {
+
+        const client = net.createConnection({host: ADDRESS, port: PORT}, () => {
+            let time = new Date();
+            console.log(time.getHours() + ':' + time.getMinutes() + ':' + time.getSeconds() + ' :: Connected to DCS server!');
+            //console.log("Connected");
+            buffer = "";
+        });
+
+        client.on('data', (data) => {
+            buffer += data;
+            while ((i = buffer.indexOf("\n")) >= 0) {
+                let data = JSON.parse(buffer.substring(0, i));
+                dataCallback(data);
+                buffer = buffer.substring(i + 1);
+            }
+        });
+
+        client.on('close', () => {
+            time = new Date();
+            console.log(time.getHours() + ':' + time.getMinutes() + ':' + time.getSeconds() + ' :: Disconnected from DCS server!');
+        });
+
+        client.on('error', () => {
+            connect();
+        });
+    }
+
+    connect();
+};
