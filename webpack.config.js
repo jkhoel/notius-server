a1673e75c3887fe8f03fe5538ca5fd283854c105
Jens Kristian Hoel <github@hoelweb.com>
2017-05-16 21:11:01 +0200

    webpack.config updated

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,30 @@
+const path = require("path");
+const webpack = require("webpack");
+
 module.exports = {
-  entry: './dev/server.js'
+  entry: "./dev/notius-server.js",
+  target: "node",
+  output: {
+    path: path.resolve(__dirname, 'bundle'),
+    filename: "notius-server.bundle.js",
+    sourceMapFilename: "notius-server.map"
+  },
+
+  plugins: [
+    new webpack.LoaderOptionsPlugin({
+      minimize: true,
+      debug: false
+    }),
+    new webpack.optimize.UglifyJsPlugin({
+      beautify: false,
+      mangle: {
+        screw_ie8: true,
+        keep_fnames: true
+      },
+      compress: {
+        screw_ie8: true
+      },
+      comments: false
+    })
+  ]
 };
